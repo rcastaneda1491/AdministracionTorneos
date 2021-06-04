6ef21b48019c458c86285d5750f2d9029de96ce6
Albin <you@example.comalbincordero2000@gmail.com  git config --global user.email you@example.comalbincordero2000@gmail.com>
2021-06-04 14:34:56 -0600

    Actualizacion2.0

--- AdminitracionDeTorneosP/Program.cs
+++ AdminitracionDeTorneosP/Program.cs
@@ -17,7 +17,7 @@ namespace AdminitracionDeTorneosP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Bienvenida());
+            Application.Run(new ViewLogin());
         }
     }
 }
