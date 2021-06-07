2af9ace25f8da75a3ac9cdfa3c22c2baf5ed51cd
rcastaneda1491 <66752435+rcastaneda1491@users.noreply.github.com>
2021-06-07 11:12:54 -0600

    Delete AdminitracionDeTorneosP directory

--- AdminitracionDeTorneosP/Model/EQUIPO_TORNEO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdminitracionDeTorneosP.Model
-{
-    public class EQUIPO_TORNEO
-    {
-        public int Id_Torneo { get; set; }
-        public int Id_Equipo { get; set; }
-        public decimal costoInscripcion { get; set; }
-    }
-}
